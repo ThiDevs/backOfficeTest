d37e36333ff0759412a3e0bce378530450b6bdf6
Thiago <thiagofelicio@hotmail.com>
2023-02-23 20:10:49 -0300

    Adicionar arquivos de projeto.

---
+++ backOfficeTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+     
+
+        public WeatherForecastController()
+        {
+}
+    }
+}
